1edc03b9175ccf109eaba265e9445bd25c83e0f6
lagranmoon <tanliqun1999@163.com>
2017-08-12 17:58:14 +0800

    UI布局完成

--- app/src/main/java/com/lagranmoon/codeditor/MainActivity.java
+++ app/src/main/java/com/lagranmoon/codeditor/MainActivity.java
@@ -1,13 +1,86 @@
 package com.lagranmoon.codeditor;
 
+import android.graphics.Color;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  {
+
+    private DrawerLayout drawer;
+    private Toolbar toolbar;
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)){
+            drawer.closeDrawer(GravityCompat.START);
+        }else {
+            super.onBackPressed();
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar_main);
+        setSupportActionBar(toolbar);
+
+        setWindowStatus();
+        initDrawer();
     }
+
+    private void initDrawer() {
+        drawer = (DrawerLayout) findViewById(R.id.drawer_main);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawer,toolbar,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_main);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.local_storage:
+                        drawer.closeDrawer(GravityCompat.START);
+                        break;
+                    case R.id.dropbox:
+                        drawer.closeDrawer(GravityCompat.START);
+                        break;
+                    case R.id.settings:
+                        drawer.closeDrawer(GravityCompat.START);
+                        break;
+                    case R.id.feedback:
+                        drawer.closeDrawer(GravityCompat.START);
+                        break;
+                    case R.id.about:
+                        drawer.closeDrawer(GravityCompat.START);
+                        break;
+                }
+
+                return true;
+            }
+        });
+    }
+
+    private void setWindowStatus() {
+        Window window = getWindow();
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.setStatusBarColor(Color.TRANSPARENT);
+    }
+
+
 }
