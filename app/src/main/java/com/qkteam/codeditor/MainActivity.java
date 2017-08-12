a730012d461c175060f0caa92eed2dcd0b930b45
lagranmoon <tanliqun1999@163.com>
2017-08-12 19:39:34 +0800

    修复修改包名后的一个小问题

--- app/src/main/java/com/qkteam/codeditor/MainActivity.java
+++ app/src/main/java/com/qkteam/codeditor/MainActivity.java
@@ -13,6 +13,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity  {
 
