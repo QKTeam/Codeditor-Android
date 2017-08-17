70191ed115bf318cec8ca8d685f0b72f8d232601
logiblack <1477949974@qq.com>
2017-08-17 20:47:46 +0800

    高亮优化，显示行数实现

--- app/src/main/java/com/qkteam/codeditor/CodeEditor.java
+++ app/src/main/java/com/qkteam/codeditor/CodeEditor.java
@@ -1,15 +1,21 @@
 package com.qkteam.codeditor;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.os.Handler;
 import android.support.v4.content.ContextCompat;
 import android.text.Editable;
+import android.text.Layout;
 import android.text.SpannableStringBuilder;
 import android.text.Spanned;
 import android.text.TextWatcher;
 import android.text.style.BackgroundColorSpan;
 import android.text.style.ForegroundColorSpan;
 import android.util.AttributeSet;
+import android.util.TypedValue;
+import android.view.ViewTreeObserver;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -25,37 +31,47 @@ public class CodeEditor extends android.support.v7.widget.AppCompatEditText {
             "short|int|long|float|double|char|String|void|boolen|integer|List"+
             "if|else|for|foreach|switch|case|defalut|break|do|while|"+
             "scanf|printf|println|return|System|Out|"+
-            "class|interface|abstract|"
+            "class|interface|abstract|private|public|protected|firiendly|static|final"
             +")\\b");
     //头文件等的匹配
-    public static Pattern PATTERN_PREPROCESSOR = Pattern.compile("[\\t]*(#include|#define|import|package|extends|implements)\\b");
+    public static Pattern PATTERN_PREPROCESSOR = Pattern.compile("[\\t]*(#include|#define|import|package|extends|implements).*");
     //注释的匹配
-    public static Pattern PATTERN_COMMENT = Pattern.compile("/\\*(?:.*|[\\n\\r])*?\\*|//.*");
+    public static Pattern PATTERN_COMMENT_GREY = Pattern.compile("//.*");
+    public static Pattern PATTERN_COMMENT_GREEN = Pattern.compile("\\B/\\*(?:.*|[\\n\\r]*)?\\*/\\B");
 
-    private Context context = getContext();
     private Handler handler = new Handler();
     private Runnable runnable = new Runnable() {
         @Override
         public void run() {
             Editable e = getText();
 //            if (!isModified)
-//                return;
+//                return; 
             highLight(e);
         }
     };
 
-    public CodeEditor(Context context) {
-        super(context);
-        this.context = context;
-        init();
-    }
+    private Paint paint = new Paint();
+    private Layout layout;
+    private Context context = getContext();
+
+
     public CodeEditor(Context context, AttributeSet attrs) {
         super(context, attrs);
+        paint.setColor(Color.parseColor("#bbbbbb"));
+        paint.setAntiAlias(false);
+        paint.setTextSize(getPixels(14));
+        paint.setStyle(Paint.Style.FILL);
+        getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                layout = getLayout();
+            }
+        });
         init();
     }
-    public CodeEditor(Context context, AttributeSet attrs, int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
-        init();
+
+    private float getPixels(int dp) {
+        return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, context.getResources().getDisplayMetrics());
     }
 
     private void init(){
@@ -77,10 +93,15 @@ public class CodeEditor extends android.support.v7.widget.AppCompatEditText {
         });
     }
 
+    /**
+     * 读取文件内容是高亮
+     * @param text: 从文件读取的文本内容*/
     private void setHighLightText(CharSequence text){
         setText(highLight(new SpannableStringBuilder(text)));
     }
 
+    /**
+     * 高亮输入的内容*/
     private Editable highLight(Editable e){
 
         if (e.length() == 0){
@@ -93,8 +114,12 @@ public class CodeEditor extends android.support.v7.widget.AppCompatEditText {
         for (Matcher m = PATTERN_KEY_WORDS.matcher(e); m.find();){
             e.setSpan(new ForegroundColorSpan(ContextCompat.getColor(context, R.color.highLightKeyword)), m.start(), m.end(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
         }
-        for (Matcher m = PATTERN_COMMENT.matcher(e); m.find();){
-            e.setSpan(new ForegroundColorSpan(ContextCompat.getColor(context, R.color.highLightComment)), m.start(), m.end(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        for (Matcher m = PATTERN_COMMENT_GREY.matcher(e); m.find();){
+            e.setSpan(new ForegroundColorSpan(ContextCompat.getColor(context, R.color.highLightCommentGrey)), m.start(), m.end(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        }
+
+        for (Matcher m = PATTERN_COMMENT_GREEN.matcher(e); m.find();){
+            e.setSpan(new ForegroundColorSpan(ContextCompat.getColor(context, R.color.highLightCommentGreen)), m.start(), m.end(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
         }
         return e;
     }
@@ -102,10 +127,7 @@ public class CodeEditor extends android.support.v7.widget.AppCompatEditText {
     private void clearSpan(Editable e) {
         // remove foreground color spans
         {
-            ForegroundColorSpan spans[] = e.getSpans(
-                    0,
-                    e.length(),
-                    ForegroundColorSpan.class);
+            ForegroundColorSpan spans[] = e.getSpans(0, e.length(), ForegroundColorSpan.class);
 
             for (int n = spans.length; n-- > 0; )
                 e.removeSpan(spans[n]);
@@ -113,14 +135,28 @@ public class CodeEditor extends android.support.v7.widget.AppCompatEditText {
 
         // remove background color spans
         {
-            BackgroundColorSpan spans[] = e.getSpans(
-                    0,
-                    e.length(),
-                    BackgroundColorSpan.class);
+            BackgroundColorSpan spans[] = e.getSpans(0, e.length(), BackgroundColorSpan.class);
 
             for (int n = spans.length; n-- > 0; )
                 e.removeSpan(spans[n]);
         }
     }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        int positionY = getBaseline();
+        drawNumber(canvas, layout.getLineLeft(0), positionY, 0);
+        for(int i=1; i<getLineCount(); i++){
+            positionY += layout.getLineBaseline(i) - layout.getLineBaseline(i-1);
+            drawNumber(canvas, layout.getLineLeft(i), positionY, i);
+        }
+
+    }
+
+    private void drawNumber(Canvas canvas, float x, float y, int line) {
+        canvas.drawText(""+(line+1), ((int)x)+getPixels(2), y, paint);
+    }
+
 }
 
